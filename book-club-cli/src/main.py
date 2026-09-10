79e53cf581380025c514a1ba9c1500010ffd7733
Leokadiia Shuhurova <leokadiiashuhurova@Leokadiias-MacBook-Air.local>
2026-09-10 15:14:21 +0300

    book-club-cli v2 json+dataclass+enum

--- book-club-cli/src/main.py
+++ book-club-cli/src/main.py
@@ -1,66 +1,58 @@
-books = [
-    {
-        "title": "The Great Gatsby", 
-        "author": "F. Scott Fitzgerald", 
-        "status": "to-read"
-    },
-    {
-        "title": "To Kill a Mockingbird", 
-        "author": "Harper Lee", 
-        "status": "read"
-    },
-    {
-        "title": "1984", 
-        "author": "George Orwell", 
-        "status": "to-read"
-    },
-]
-book_statuses = {"read", "to-read", "currently-reading"}
+from dataclasses import dataclass
+from enum import Enum
+import json
 
-def add_book(title, author):
-    books.append({
-        "title": title, 
-        "author": author, 
-        "status": "to-read"
-    })
-    print(f"Book '{title}' by '{author}' added.")
+class Status(Enum):
+    TO_READ = "to-read"
+    CURRENTLY_READING = "currently-reading"
+    READ = "read"
 
-def remove_book(title):
+@dataclass
+class Book:
+    title: str
+    author: str
+    status: Status = Status.TO_READ
+
+def add_book(books, new_book: Book):
+    books.append(new_book)
+    print(f"Book '{new_book.title}' by '{new_book.author}' added.")
+
+def remove_book(books, title):
     for book in books:
-        if book["title"] == title:
+        if book.title == title:
             books.remove(book)
             print(f"Book '{title}' removed.")
             return
     print(f"Book '{title}' not found.")
 
-def search_book(title):
+def search_book(books, title):
     book_found = False
     for book in books:
-        if title in book["title"]:
-            print(f"- {book["title"]} by {book["author"]}")
+        if title in book.title:
+            print(f"- {book.title} by {book.author}")
             book_found = True
     
     if not book_found:
         print(f"Book '{title}' not found.")
 
-def change_status(title, new_status):
+def change_status(books, title, new_status):
     for book in books:
-        if title == book["title"]:
-            old_status = book["status"]
-            book["status"] = new_status
-            print(f"- {book["title"]} by {book["author"]}. {old_status} -> {new_status}")
+        if title == book.title:
+            old_status = book.status
+            book.status = new_status
+            print(f"- {book.title} by {book.author}. {old_status.value} -> {new_status.value}")
             return
     print(f"Book '{title}' not found.")
     
 
-def list_books():
+def list_books(books):
     if not books:
         print("No books in the list")
     else:
         for book in books:
-            print(f"- {book["title"]} by {book["author"]} ({book["status"]})")
+            print(f"- {book.title} by {book.author} ({book.status.value})")
 
-def main():
+def main(books):
     print("Welcome to Book Club!")
 
     while True:
@@ -78,32 +70,61 @@ def main():
             print("Add book")
             title = input("Enter book title: ")
             author = input("Enter book author: ")
-            add_book(title, author)
+            add_book(books, Book(title, author))
         elif choice == "2":
-            list_books()
+            list_books(books)
         elif choice == "3":
             title = input("Enter book title to remove: ")
-            remove_book(title)
+            remove_book(books, title)
         elif choice == "4":
             title = input("Enter book title to search: ")
-            search_book(title)
+            search_book(books, title)
         elif choice == "5":
             title = input("Enter book title to update status: ")
 
             while True:   
-                status = input("Enter read, currently-reading or to-read: ")
-
-                if status in book_statuses:
-                    change_status(title, status)
+                status_value = input("Enter read, currently-reading or to-read: ")
+                try:
+                    status = Status(status_value)
+                    change_status(books, title, status)
                     break
-                else:
+                except(ValueError):
                     print("Unknown command")
         elif choice == "6":
             print("Goodbye!")
-            break
+            return books
         else:
             print("Unknown command")
 
 
 if __name__ == "__main__":
-    main()
+    data_path = "../data/books.json"
+    books = []
+
+    with open(data_path, "r") as file:
+        data = json.load(file)
+
+        for book_data in data:
+            book = Book(
+                book_data["title"],
+                book_data["author"],
+                Status(book_data["status"])
+            )
+            books.append(book)
+
+    books = main(books)
+
+    data = []
+    
+    for book in books:
+        book_data = {
+            "title": book.title,
+            "author": book.author,
+            "status": book.status.value
+        }
+                
+        data.append(book_data)
+
+    with open(data_path, "w") as file:
+
+        json.dump(data, file, indent=4)
